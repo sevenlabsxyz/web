0d5aecdc79bbbbccfdee77b79bdeb1d68f603dc7
Andre Neves <andre@zebedee.io>
2023-10-20 17:00:05 -0700

    chore: work work

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,19 +6,19 @@ import { Analytics } from "./components/analytics";
 
 export const metadata: Metadata = {
 	title: {
-		default: "chronark.com",
-		template: "%s | chronark.com",
+		default: "seven.sx",
+		template: "%s | seven.sx",
 	},
-	description: "Software engineer at upstash.com and founder of planetfall.io",
+	description: "Venture Studio building for impact and change.",
 	openGraph: {
-		title: "chronark.com",
+		title: "seven.sx",
 		description:
-			"Software engineer at upstash.com and founder of planetfall.io",
-		url: "https://chronark.com",
-		siteName: "chronark.com",
+			"Venture Studio building for impact and change.",
+		url: "https://seven.sx",
+		siteName: "seven.sx",
 		images: [
 			{
-				url: "https://chronark.com/og.png",
+				url: "https://seven.sx/og.png",
 				width: 1920,
 				height: 1080,
 			},
